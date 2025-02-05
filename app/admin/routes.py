245847df38094622f61d09b8755e74a996c2195b
Vyshnav Vinod <105802389+iam-yshnav@users.noreply.github.com>
2025-02-05 21:57:06 +0530

    error correction-admin

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -4,18 +4,22 @@ admin_bp = Blueprint('admin', __name__)
 
 @admin_bp.route('/') # Yet to implement the admin access stuff 
 def admin_dashboard():
-    return render_template('admin.html')
+    from app.models import ThreatReport, User
+    threats = ThreatReport.query.all()  # Fetch all threat reports
+    users = User.query.all()  # Fetch all users
+    return render_template('admin.html', threats=threats, users=users)
+    
 
-@admin_bp.route('/threats')
-def view_threats():
-    # Fetch all threat reports from the database
-    from app.models import ThreatReport
-    threats = ThreatReport.query.all()
-    return render_template('admin.html', threats=threats)
+# @admin_bp.route('/threats')
+# def view_threats():
+#     # Fetch all threat reports from the database
+#     from app.models import ThreatReport
+#     threats = ThreatReport.query.all()
+#     return render_template('admin.html', threats=threats)
 
-@admin_bp.route('/users')
-def view_users():
-    # Fetch all users
-    from app.models import User
-    users = User.query.all()
-    return render_template('admin.html', users=users)
+# @admin_bp.route('/users')
+# def view_users():
+#     # Fetch all users
+#     from app.models import User
+#     users = User.query.all()
+#     return render_template('admin.html', users=users)
